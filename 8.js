bb1034e61810173819228a1e9ec3223f1fd8eaac
ToghrulAG <aghalitg@gmail.com>
2024-01-08 06:34:56 +0400

    solved 5 tasks deleted imports, cause they make conflicts

--- 8.js
+++ 8.js
@@ -2,6 +2,14 @@
 
 //                                            Fibonacci 
 
-let findFib = prompt('Enter Value To Find A FIB');
-let toInt = parseInt(findFib);
-console.log(typeof(toInt))
+let N = 14;   // EDED Girin  
+
+let fibo = [0, 1]; // num deyiskeninde yerine yetirdiyimiz alqoritmi (fibonaci dusturu) yazmaq ucun, ilkin olaraq 0,1 ededleri goturdum, daha sonra hesablaya hesablaya push etdim.
+
+for(let i = 2; i < N; i++) {
+    let num =  fibo[i - 1] + fibo[i - 2];  
+    fibo.push(num)
+}
+console.log(fibo)
+
+

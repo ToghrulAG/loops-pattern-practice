628e2bf35d65ccc8c97d2cffc70d1310353bbc9b
ToghrulAG <aghalitg@gmail.com>
2024-01-08 06:09:58 +0400

    added html and imported js

---
+++ 8.js
@@ -0,0 +1,7 @@
+//                                      0, 1, 1, 2, 3, 5, 8, 13, ...
+
+//                                            Fibonacci 
+
+let findFib = prompt('Enter Value To Find A FIB');
+let toInt = parseInt(findFib);
+console.log(typeof(toInt))
